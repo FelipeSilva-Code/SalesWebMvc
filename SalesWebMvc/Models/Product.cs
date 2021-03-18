d6d8f67ce2a9c06f6dee2c658a4c06adfac69776
Felipe Silva <felipecsilva208@gmail.com>
2021-03-18 20:22:14 -0300

    Seeding Service criado

--- SalesWebMvc/Models/Product.cs
+++ SalesWebMvc/Models/Product.cs
@@ -20,12 +20,13 @@ namespace SalesWebMvc.Models
         { 
         }
 
-        public Product(int id, string name, double price, string brand, Department department)
+        public Product(int id, string name, double price, string brand, int Quantity, Department department)
         {
             Id = id;
             Name = name;
             Price = price;
             Brand = brand;
+            Quantity = Quantity;
             Department = department;
         }
 
