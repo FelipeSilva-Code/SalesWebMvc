c80b7fda1e1915aa6386cf2b7cff66cb28131f74
Felipe Silva <felipecsilva208@gmail.com>
2021-03-20 13:37:26 -0300

    Nova migration com Foreign Key not null

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -13,6 +13,7 @@ namespace SalesWebMvc.Models
         public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
+        public int DepartmentId { get; set; } //References an Id of a Department 
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
     
         public Seller ()
