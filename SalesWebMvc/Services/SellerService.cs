abe9db168e14ab83835f34d02bde3a7441d5a8e4
Felipe Silva <felipecsilva208@gmail.com>
2021-03-20 18:11:07 -0300

    Acao de deletar seller criada

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -27,5 +27,16 @@ namespace SalesWebMvc.Services
             _context.Add(seller);
             _context.SaveChanges();
         }
+
+        public Seller FindById (int id)
+        {
+            return _context.Seller.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Remove (int id)
+        {
+            _context.Remove(FindById(id));
+            _context.SaveChanges();
+        }
     }
 }
