d43795c93f6867a0b9ac5c504e9bd94a5f2c0edf
Felipe Silva <felipecsilva208@gmail.com>
2021-03-22 17:12:40 -0300

    SimpleSearch - Finalizado

--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 using SalesWebMvc.Models.Enuns;
 
 namespace SalesWebMvc.Models
@@ -9,6 +10,8 @@ namespace SalesWebMvc.Models
     public class SalesRecord
     {
         public int Id { get; set; }
+        
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
         public double Amount { get; set; }
         public SaleStatus Status { get; set; }
---
+++ SalesWebMvc/Services/SalesRecordServices.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMvc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace SalesWebMvc.Services
+{
+    public class SalesRecordServices
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public SalesRecordServices(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+
+            var result = await _context.SalesRecord
+                         .Include(x => x.Product).Include(x => x.Product.Department)
+                         .Include(x => x.Seller).Where(x => x.Date >= minDate
+                          && x.Date <= maxDate).ToListAsync();
+
+            return result.OrderByDescending(x => x.Date).ToList();
+
+        }
+    }
+}
