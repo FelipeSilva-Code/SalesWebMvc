79f75f8c5489e87326dd2562149ec443d21c4c07
Felipe Silva <felipecsilva208@gmail.com>
2021-03-23 16:47:23 -0300

    Crud no Product - completo

--- SalesWebMvc/Controllers/ProductsController.cs
+++ SalesWebMvc/Controllers/ProductsController.cs
@@ -16,12 +16,12 @@ namespace SalesWebMvc.Controllers
         private readonly ProductService _productService;
         private readonly DepartmentService _departmentService;
 
-        public ProductsController (ProductService productService, DepartmentService departmentService)
+        public ProductsController(ProductService productService, DepartmentService departmentService)
         {
             _productService = productService;
             _departmentService = departmentService;
         }
-       
+
         public async Task<IActionResult> Index()
         {
             var products = await _productService.FindAllAsync();
@@ -37,9 +37,9 @@ namespace SalesWebMvc.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create (Product product)
+        public async Task<IActionResult> Create(Product product)
         {
-            if(!ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
                 var departments = await _departmentService.FindAllAsync();
                 var productForm = new ProductFormViewModel() { Departments = departments };
@@ -51,12 +51,95 @@ namespace SalesWebMvc.Controllers
                 await _productService.InsertAsync(product);
                 return RedirectToAction(nameof(Index));
             }
-            catch(EqualException e)
+            catch (EqualException e)
             {
                 return RedirectToAction(nameof(Error), new { message = e.Message });
             }
 
         }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
+
+            var obj = await _productService.FindByIdAsync(id.Value);
+
+            if (obj == null)
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                await _productService.RemoveAsync(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (IntegrityException e)
+            {
+                return RedirectToAction(nameof(Error), new { message = e.Message });
+            }
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
+
+            var obj = await _productService.FindByIdAsync(id);
+
+            if (obj == null)
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
+
+            return View(obj);
+        }
+
+        public async Task<IActionResult>  Edit(int? id)
+        {
+            if(id == null)
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
+
+            var departments = await _departmentService.FindAllAsync();
+            var product = await _productService.FindByIdAsync(id);
+           
+            if(product == null)
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
+
+            var productForm = new ProductFormViewModel() { Departments = departments, Product = product };
+            return View(productForm);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit (int? id, Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                var departments = await _departmentService.FindAllAsync();
+                var productForm = new ProductFormViewModel() { Departments = departments, Product = product };
+                return View(productForm);
+            }
+
+            if(id != product.Id)
+                return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
+
+            try
+            {
+                await _productService.UpdateAsync(product);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (ApplicationException e)
+            {
+                return RedirectToAction(nameof(Error), new { message = e.Message });
+            }
+        }
+
+
 
 
         public IActionResult Error(string message)
--- SalesWebMvc/Services/ProductService.cs
+++ SalesWebMvc/Services/ProductService.cs
@@ -39,5 +39,43 @@ namespace SalesWebMvc.Services
             _context.Add(product);
             await _context.SaveChangesAsync();
         }
+
+        public async Task<Product> FindByIdAsync (int? id)
+        {
+            var product = await _context.Product.Include(x => x.Department)
+                                        .FirstOrDefaultAsync(x => x.Id == id);
+            return product;
+        }
+
+        public async Task RemoveAsync (int id)
+        {
+            try
+            {
+                var product = await FindByIdAsync(id);
+                _context.Product.Remove(product);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                throw new IntegrityException("Can't delete product because it is on sales");
+            }
+        }
+
+        public async Task UpdateAsync (Product product)
+        {
+            if (!await _context.Seller.AnyAsync(x => x.Id == product.Id))
+                throw new NotFoundException("Id not found");
+
+            try
+            {
+                _context.Product.Update(product);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
+
+        }
     }
 }
